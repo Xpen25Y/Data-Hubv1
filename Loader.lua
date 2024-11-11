3d61afebe046d977dccd315dd5a86f3648acbfd5
Xpen25Y <122020825+Xpen25Y@users.noreply.github.com>
2024-11-11 16:04:25 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,30 @@
-# Data-Hubv1
+if _G.KaitunMode then
+    local gamelist = {
+        [994732206] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/KaitunBloxFruits"
+    }
+    local checkgame = gamelist[game.gameId]
+
+    if gamelist[game.gameId] then
+        getgenv().AuthTime = tick()
+        print(checkgame)
+        loadstring(game:HttpGet(checkgame))()
+    else
+        game.Players.LocalPlayer:Kick("Game Not Support")
+    end
+else
+    local gamelist = {
+        [4777817887] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BladeBall", -- BB
+        [5750914919] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/Fisch", -- Fisch
+        [994732206] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BloxFruits", -- BF
+        [1202096104] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/Drivingemprie", -- DE
+    }
+    local checkgame = gamelist[game.gameId]
+
+    if gamelist[game.gameId] then
+        getgenv().AuthTime = tick()
+        print(checkgame)
+        loadstring(game:HttpGet(checkgame))()
+    else
+        game.Players.LocalPlayer:Kick("Game Not Support")
+    end
+end
