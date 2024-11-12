35b13c476b44a3a27cdda75be49d0081ba1794e1
Xpen25Y <122020825+Xpen25Y@users.noreply.github.com>
2024-11-12 19:29:12 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -14,8 +14,8 @@ if _G.KaitunMode then
 else
     local gamelist = {
         [4777817887] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BladeBall", -- BB
-        [5750914919] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/Fisch", -- Fisch
-        [994732206] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BloxFruits", -- BF
+        [5750914919] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Fisch.lua", -- Fisch
+        [994732206] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Blox-Fruits.lua", -- BF
         [1202096104] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/Drivingemprie", -- DE
         [537413528] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BuildABoatForTreasure", -- 
     }
