3a87e29aa8d4ec51c7dc8f1195695262c909c89a
Xpen25Y <122020825+Xpen25Y@users.noreply.github.com>
2024-11-14 20:43:09 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -15,7 +15,7 @@ else
     local gamelist = {
         [4777817887] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BladeBall", -- BB
         [5750914919] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Fisch.lua", -- Fisch
-        [994732206] = "https://raw.githubusercontent.com/xZcAtliftz/Data-Hub/main/Blox-Fruits.lua", -- BF
+        [994732206] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BloxFruits", -- BF
         [1202096104] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/Drivingemprie", -- DE
         [537413528] = "https://raw.githubusercontent.com/Xpen25Y/Data-Hubv1/refs/heads/main/BuildABoatForTreasure", -- 
     }
